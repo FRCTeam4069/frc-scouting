01989afaf554355b8a948c52da620945f6e30d5d
brendon-ai <brendon-m@outlook.com>
2017-10-25 17:45:31 -0400

    Added example code using XAMPP

---
+++ htdocs/frc/frc_routines.js
@@ -0,0 +1,80 @@
+function sendpage1data() {
+  var teamnum = $('#teamnum').val();
+  var canthrow = $('#canthrow').is(':checked');
+  var canclimb = $('#canclimb').is(':checked');
+  var teamcolor = $('#color').val();
+  var notes = $('#notes').val();
+
+  var request = $.ajax(
+    {
+      url: "page1save.php",
+      type: "post",
+      data:
+      {
+        "tnum": teamnum,
+        "throw": canthrow,
+        "climb": canclimb,
+        "color": teamcolor,
+        "tnotes": notes
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        alert("Status: " + textStatus);
+        alert("Error: " + errorThrown);
+      },
+      success: function (data) {
+        console.log(data)
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        var pj = $.parseJSON(data);
+        var whatarepoodles = pj.poodles;
+        var succsess = pj.success;
+        alert("Poodles: " + whatarepoodles);
+
+        var boo = 1;
+      }
+    });
+}
+
+
+
+
+
+
+
+function sendthedata() {
+  var bike = $('#bikeval').is(':checked');
+  var car = $('#carval').is(':checked');
+  var msg = $('#message').val();
+
+  var request = $.ajax(
+    {
+      url: "demo_test.php",
+      type: "post",
+      data:
+      {
+        "thebikevalue": bike,
+        "thecarvalue": car,
+        "themessagevalue": msg
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+        alert("Status: " + textStatus);
+        alert("Error: " + errorThrown);
+      },
+      success: function (data) {
+        if (data.error) {
+          alert(data.error);
+          return;
+        }
+        var pj = $.parseJSON(data);
+        var whatarepoodles = pj.poodles;
+        var succsess = pj.success;
+        alert("Poodles: " + whatarepoodles);
+
+        var boo = 1;
+      }
+    });
+}
+
+
---
+++ htdocs/frc/page1save.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+  $fp=fopen("page1save.out","wb");
+  fwrite($fp,"Opening database connection...\n");
+  $con = mysqli_connect("localhost","root","","frc4069");
+  if (!$con)
+  {
+      fwrite($fp,"Could not connect to database\n");
+      fclose();
+  		die('Could not connect: ' . mysql_error());
+  }
+
+$tnum = $_POST['tnum'];
+$throw = $_POST['throw'];
+$climb = $_POST['climb'];
+$color = $_POST['color'];
+$tnotes = $_POST['tnotes'];
+
+$abilities=0;
+if ($climb=='true')
+  $abilities=1;
+if ($throw=='true')
+  $abilities |= 2;
+
+fwrite($fp,"tnum=".$tnum.",throw=".$throw.",climb=".$climb.",color=".$color.",notes=".$tnotes.",abilities=".$abilities."\n");
+
+
+$sql="insert into teaminfo (teamnumber,notes,color,abilities) values ('".$tnum."','".$tnotes."','".$color."',".$abilities.")";
+
+fwrite($fp,"sql=".$sql."\n");
+$result = $con->query($sql);
+
+echo '{"success":"true","poodles":"arefurry"}';
+fclose($fp);
+mysqli_close($con);//Close the DB Connection
+?>
