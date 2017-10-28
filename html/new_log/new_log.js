7cbc6a92dbc52e926847113858b8e212b1a55536
brendon-ai <brendon-m@outlook.com>
2017-10-28 10:53:53 -0400

    move new_log_save.py to cgi_bin

--- html/new_log/new_log.js
+++ html/new_log/new_log.js
@@ -5,35 +5,12 @@ function save_log_data() {
   var teamcolor = $('#color').val();
   var notes = $('#notes').val();
 
-  var request = $.ajax(
-    {
-      url: "new_log_save.py",
-      type: "post",
-      data:
-      JSON.stringify({
-        "tnum": teamnum,
-        "throw": canthrow,
-        "climb": canclimb,
-        "color": teamcolor,
-        "tnotes": notes
-      }),
-      dataType: "json",
-      error: function (XMLHttpRequest, textStatus, errorThrown) {
-        alert("Status: " + textStatus);
-        alert("Error: " + errorThrown);
-      },
-      success: function (data) {
-        if (data.error) {
-          alert(data.error);
-          return;
-        }
-        var pj = $.parseJSON(data);
-        var success = pj.success;
-        if (success == true) {
-          alert("Log recorded.");
-        }
+  var request = $.ajax({
+    type: "POST",
+    url: "/cgi_bin/new_log_save.py",
+    data: { 'param': 'hello' },
 
-        var boo = 1;
-      }
-    });
+  }).done(function (o) {
+    console.log(o)
+  })
 }
