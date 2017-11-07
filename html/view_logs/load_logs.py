057c17b8a88d1d07c992dda52aa02ec8c0c7786c
brendon-ai <brendon-m@outlook.com>
2017-11-07 08:40:04 -0500

    Mostly complete loading script

--- html/view_logs/load_logs.py
+++ html/view_logs/load_logs.py
@@ -3,4 +3,21 @@
 import os
 import sys
 
-# Code goes here
+# File path of the CSV file to save data entries to
+DATA_PATH = os.path.expanduser('~/Developer/frc-scouting/data.csv')
+
+# Print the JSON output type, followed by a newline
+print('Content-Type: application/json\n')
+
+# Load all of the lines from the file
+with open(DATA_PATH) as data_file:
+    data_lines = data_file.readlines()
+
+# Iterate over each of the data lines
+for line in data_lines:
+
+    # Remove whitespace from the beginning and end of the line
+    line = line.strip()
+
+    # Print the unprocessed line, sending it to the JavaScript calling this script
+    print(line)
